433dcbd99439f0ea947b4708a03a132bd1da784b
ecamposh <82472401+ecamposh@users.noreply.github.com>
2025-06-11 19:19:58 -0600

    rocky9.sh

---
+++ rocky9.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+nmcli networking on
+dnf config-manager --set-disabled epel-cisco-openh264
+dnf clean packages
+dnf repolist
+dnf remove -y xe-guest-utilities
+dnf install -y xe-guest-utilities-latest
+systemctl start xe-linux-distribution
+systemctl enable xe-linux-distribution
+systemctl restart nova-agent
+dnf install -y wget vim nano
+dnf --nogpg install https://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el9.noarch.rpm
+dnf repolist
+dnf -y install redhat-lsb-core
