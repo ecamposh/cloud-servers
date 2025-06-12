74050357577c671d3ac29bf426610da89e65dc92
ecamposh <82472401+ecamposh@users.noreply.github.com>
2025-06-11 19:18:44 -0600

    alma9.sh

---
+++ alma9.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+nmcli networking on
+dnf config-manager --set-disabled epel-cisco-openh264
+dnf clean packages
+dnf repolist
+dnf remove -y xe-guest-utilities
+dnf install -y xe-guest-utilities-latest
+systemctl start xe-linux-distribution
+systemctl enable xe-linux-distribution
+systemctl restart nova-agent
+dnf install -y wget vim nano
+dnf install -y almalinux-release-devel
+dnf repolist
+dnf -y install redhat-lsb-core
