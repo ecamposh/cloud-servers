6efacbabb7c13e60dcda38765ea8b6d7e56bfbf3
ecamposh <82472401+ecamposh@users.noreply.github.com>
2025-06-27 17:02:48 -0600

    rocky9.sh

--- rocky9.sh
+++ rocky9.sh
@@ -9,6 +9,6 @@ systemctl start xe-linux-distribution
 systemctl enable xe-linux-distribution
 systemctl restart nova-agent
 dnf install -y wget vim nano
-dnf --nogpg install https://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el9.noarch.rpm
+dnf --nogpg install -y https://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el9.noarch.rpm
 dnf repolist
 dnf -y install redhat-lsb-core
