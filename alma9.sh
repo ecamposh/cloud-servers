8cfb2e00fe864997cf923a6555805be8cc6a969d
ecamposh <82472401+ecamposh@users.noreply.github.com>
2025-06-27 18:23:05 -0600

    alma9.sh

--- alma9.sh
+++ alma9.sh
@@ -8,7 +8,7 @@ dnf install -y xe-guest-utilities-latest
 systemctl start xe-linux-distribution
 systemctl enable xe-linux-distribution
 systemctl restart nova-agent
-dnf install -y wget vim nano
+dnf install -y curl wget vim nano
 dnf install -y almalinux-release-devel
 dnf repolist
 dnf -y install redhat-lsb-core
