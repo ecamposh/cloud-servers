990bfe9da3de89327b2a893a1f9c3da068644bc8
ecamposh <82472401+ecamposh@users.noreply.github.com>
2026-04-27 21:44:33 -0600

    centos6_3-no-official-vault-repos.sh

--- centos6_3-no-official-vault-repos.sh
+++ centos6_3-no-official-vault-repos.sh
@@ -7,34 +7,27 @@
 ~$ vi /etc/yum.repos.d/CentOS-Vault.repo
 
 [base]
-name=CentOS-6 - Base - Vault
-baseurl=http://linuxsoft.cern.ch/centos-vault/6.10/os/$basearch/
-gpgcheck=0
-#gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
-enabled=1
-
-[os]
-name=CentOS-6 - OS - Vault
-baseurl=http://linuxsoft.cern.ch/centos-vault/6.10/os/$basearch/
+name=CentOS-6.5 - Base - Vault
+baseurl=http://linuxsoft.cern.ch/centos-vault/6.5/os/$basearch/
 gpgcheck=0
 #gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
 enabled=1
 
 [updates]
-name=CentOS-6 - Updates - Vault
-baseurl=http://linuxsoft.cern.ch/centos-vault/6.10/updates/$basearch/
+name=CentOS-6.5 - Updates - Vault
+baseurl=http://linuxsoft.cern.ch/centos-vault/6.5/updates/$basearch/
 gpgcheck=0
 #gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
 enabled=1
 
 [extras]
-name=CentOS-6 - Extras - Vault
-baseurl=http://linuxsoft.cern.ch/centos-vault/6.10/extras/$basearch/
+name=CentOS-6.5 - Extras - Vault
+baseurl=http://linuxsoft.cern.ch/centos-vault/6.5/extras/$basearch/
 gpgcheck=0
 #gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
 enabled=1
 
 
 ### Other options
-https://archive.kernel.org/centos-vault/6.10/
-https://mirror.nsc.liu.se/centos-store/6.10/
+https://archive.kernel.org/centos-vault/6.5/
+https://mirror.nsc.liu.se/centos-store/6.5/
